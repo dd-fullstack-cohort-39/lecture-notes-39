d51c348386c0fb28da05aecbc8879ee363f6d012
martybonacci <martybonacci@gmail.com>
2022-07-18 08:48:53 -0600

    arrow functions

---
+++ code-alongs/more-functions/arrow-functions.js
@@ -0,0 +1,11 @@
+// conventional function syntax
+function displayName (name){
+  console.log(name)
+}
+displayName('Fred')
+
+// arrow function syntax
+let displayLabel = (text) => {
+  console.log(text)
+}
+displayLabel('Arrow')
